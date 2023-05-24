f32a8a54da2bab8317027473c8c19d41918532a7
Andrew Cranford <acranford25@gmail.com>
2023-05-24 09:37:43 -0400

    working on routines

--- db/seed.js
+++ db/seed.js
@@ -8,14 +8,47 @@ const {
 
 async function dropTables() {
   // Drop all tables in order
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
 }
 
 async function createTables() {
   // Define your tables and fields
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        creater_id INTEGER REFERENCES users(id),
+        is_public BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULLS
+      )`);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
 }
 
 async function populateTables() {
   // Seed tables with dummy data from seedData.js
+  users;
 }
 
 async function rebuildDb() {
