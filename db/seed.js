cb648bc36b2714c3dedfc85523e1441f7f7d89d3
c-b94 <cedricboudreaux@yahoo.com>
2023-05-25 10:11:11 -0500

    activities adapters

--- db/seed.js
+++ db/seed.js
@@ -6,15 +6,16 @@ const {
   routine_activities,
 } = require("./seedData");
 const {createUser, getAllUsers,getUserById,getUserByUsername,getUser} = require("/home/cxb94/dev/FitnessTrackerBackend/db/adapters/users.js");
-
+const {createActivity, getActivityById, getAllActivities, updateActivity} = require("/home/cxb94/dev/FitnessTrackerBackend/db/adapters/activities.js");
 async function dropTables() {
-  // Drop all tables in order
+  // Drop all tables in orderrs
   try {
     console.log("Starting to drop tables...");
 
     // have to make sure to drop in correct order
     await client.query(`
       DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS activities;
     `);
 
     console.log("Finished dropping tables!");
@@ -38,6 +39,16 @@ async function createTables() {
       );
     `);
 
+    await client.query(`
+      CREATE TABLE  activities(
+        id SERIAL PRIMARY KEY,
+        name varchar(255) UNIQUE NOT NULL,
+        description text NOT NULL
+      );
+    `);
+
+    
+
     console.log("Finished building tables!");
   } catch (error) {
     console.error("Error building tables!");
@@ -48,6 +59,7 @@ async function createTables() {
 async function populateTables() {
   // Seed tables with dummy data from seedData.js
   const populateUsers = users.map(async user => await createUser(user));
+  const populateActivities = activities.map(async act => await createActivity(act));
 }
 
 async function rebuildDb() {
@@ -71,7 +83,18 @@ async function rebuildDb() {
     console.log("Calling getuser with hanz");
     const result3 = await getUser("hanz","test3");
     console.log("Result:", result3);
+    console.log("////////////////// tetsting Activities///////////////////");
+    console.log("Calling getAllActivities");
+    const activities = await getAllActivities();
+    console.log("Result:", activities);
 
+    console.log("Calling getActivityById with 1");
+    const result4 = await getActivityById(1);
+    console.log("Result:", result4);
+
+    console.log("Calling UpdateActivity");
+    const result5 = await updateActivity(1,"armwrestle","test your might");
+    console.log("Result:", result5);
 
   } catch (error) {
     console.error(error);
